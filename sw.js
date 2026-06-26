d79d4e4a9bf9e0a3b8d8688b06875f1189a80b4e
contep2k15-oss <contep2k15@gmail.com>
2026-06-26 13:40:16 +0700

    Update service worker to use new cache version

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Nội Bài Car – Service Worker v1.0
-const CACHE = 'noibaicar-v1';
+const CACHE_NAME = 'nbc-v2.3';
+
 const ASSETS = [
   './',
   './index.html',
@@ -8,23 +8,44 @@ const ASSETS = [
   './icons/icon-512.png'
 ];
 
-self.addEventListener('install', e => {
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(ASSETS);
+    })
   );
 });
 
-self.addEventListener('activate', e => {
+self.addEventListener('activate', function(e) {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE_NAME; })
+            .map(function(k) { return caches.delete(k); })
+      );
+    }).then(function() {
+      return self.clients.claim();
+    })
   );
 });
 
-self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
+self.addEventListener('fetch', function(e) {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
+    fetch(e.request)
+      .then(function(res) {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(function(cache) {
+          cache.put(e.request, clone);
+        });
+        return res;
+      })
+      .catch(function() {
+        return caches.match(e.request);
+      })
   );
+});
+
+self.addEventListener('message', function(e) {
+  if (e.data === 'skipWaiting') self.skipWaiting();
 });
